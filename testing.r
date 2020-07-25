b1ce9e0261fc6831a56911b8825e2174e859a612
isdi <ishtiaq247@gmail.com>
2020-07-25 10:37:19 +0200

    testing.r
    First file on github

---
+++ testing.r
@@ -0,0 +1,11 @@
+# This is from Github
+print("Hello from Github")
+
+name= "Musa"
+version=" Rstudio - in Writing"
+
+print(name)
+print("These lines are written in Rstudio")
+
+print("I love parents")
+
